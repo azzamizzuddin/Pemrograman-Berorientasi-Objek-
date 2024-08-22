0659f4f9c85a5578a95fa2b6f47acafa49aa068d
azzamizzuddin <mazazzuu@gmail.com>
2024-08-23 06:07:06 +0700

    Add files via upload

---
+++ soalno1/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Deklarasi dan inisialisasi variabel bertipe byte
+        byte angka1 = 125;
+        byte angka2 = 6;
+
+        // Melakukan penjumlahan dan casting ke tipe byte
+        byte hasil = (byte) (angka1 + angka2);
+
+        // Mencetak hasil ke konsol
+        System.out.println("Hasil 1 = " + hasil);
+    }
+}
---
+++ soalno2/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 51;
+        String s = (i < 40) ? "life" : (i > 50) ? "universe" : "everything";
+        System.out.println(s);
+    }
+}
